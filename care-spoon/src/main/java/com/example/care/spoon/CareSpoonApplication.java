214cef189ecb522c240b63ae39c257474765f696
sohyundoh <lyny123@naver.com>
2023-02-13 20:47:09 +0900

    feat : initial setting

---
+++ care-spoon/src/main/java/com/example/care/spoon/CareSpoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.care.spoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareSpoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareSpoonApplication.class, args);
+	}
+
+}
