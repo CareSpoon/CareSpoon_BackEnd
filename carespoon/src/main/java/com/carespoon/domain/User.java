3552322ad9a0f9a5e16bbbe99a3cb3690a659cee
sohyundoh <lyny123@naver.com>
2023-02-15 12:51:11 +0900

    feat: user domain initial setting
    spring security 와 관련해서 할 예정

--- carespoon/src/main/java/com/carespoon/domain/User.java
+++ carespoon/src/main/java/com/carespoon/domain/User.java
@@ -1,2 +1,34 @@
-package com.carespoon.domain;public class User {
+package com.carespoon.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 400, nullable = false)
+    private String username;
+
+    @Column(length = 500, nullable = false)
+    private String email;
+
+    @Column(length = 500, nullable = false)
+    private String password;
+
+    @Builder
+    public User(String username, String email, String password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 }
