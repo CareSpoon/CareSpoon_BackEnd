930e72c283695f6a8facbfc8aa6d652f5eb11166
sohyundoh <lyny.doh@gmail.com>
2023-03-06 17:48:56 +0900

    feat: querydsl sum API 개발 완료

--- carespoon/src/main/java/com/carespoon/controller/OneMealController.java
+++ carespoon/src/main/java/com/carespoon/controller/OneMealController.java
@@ -1,12 +1,18 @@
 package com.carespoon.controller;
 
+import com.carespoon.Repository.OneMealRepositoryCustom;
 import com.carespoon.domain.Image;
 import com.carespoon.dto.ImageDto;
 import com.carespoon.dto.OneMealResponseDto;
 import com.carespoon.dto.OneMealSaveRequestDto;
 import com.carespoon.service.ImageService;
 import com.carespoon.service.OneMealService;
+import com.querydsl.core.Tuple;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -17,6 +23,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,6 +33,43 @@ public class OneMealController {
     private final OneMealService oneMealService;
     private ImageService imageService;
 
+    @Autowired
+    @Qualifier("oneMealRepositoryImpl")
+    private OneMealRepositoryCustom oneMealRepositoryCustom;
+
+    @Getter
+    @Setter
+    public static class Daily{
+        private LocalDate date;
+        private Integer totalKcal;
+        private Integer totalFat;
+        private Integer totalProtein;
+        private Integer totalCarbon;
+        public Daily(LocalDate date, Integer totalKcal, Integer totalCarbon, Integer totalFat, Integer totalProtein){
+            this.date = date;
+            this.totalKcal = totalKcal;
+            this.totalCarbon = totalCarbon;
+            this.totalFat = totalFat;
+            this.totalProtein = totalProtein;
+        }
+    }
+
+    @GetMapping("/dailynurition")
+    public List<Daily> getDaily(){
+        List<Tuple> dailyNutrition = oneMealRepositoryCustom.findOneMealByCreatedTime();
+
+        List<Daily> dailyNutritionList = new ArrayList<>();
+        for(Tuple tuple: dailyNutrition) {
+            LocalDate date = tuple.get(0,LocalDate.class);
+            Integer totalKcal = tuple.get(1, Integer.class);
+            Integer totalCarbon = tuple.get(2,Integer.class);
+            Integer totalFat = tuple.get(3,Integer.class);
+            Integer totalProtein = tuple.get(4, Integer.class);
+
+            dailyNutritionList.add(new Daily(date, totalKcal,totalCarbon, totalFat, totalProtein));
+        }
+        return dailyNutritionList;
+    }
     @PostMapping("/onemeal")
     public Long save(@RequestBody OneMealSaveRequestDto requestDto) {
 
