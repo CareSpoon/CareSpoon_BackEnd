13859e68e87e5f7f03ab08f0084008b69ec86989
sohyundoh <lyny.doh@gmail.com>
2023-05-18 22:16:09 +0900

    feat:  UuidResponseDto
    # 형식 : [feat/#issue number]
    # 커밋 예시 : [feat/#3] home 뷰 구현
    
    ############################
    # feat : 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refactor : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    ############################

--- carespoon/src/main/java/com/carespoon/user/controller/UserController.java
+++ carespoon/src/main/java/com/carespoon/user/controller/UserController.java
@@ -2,9 +2,12 @@ package com.carespoon.user.controller;
 
 
 import com.carespoon.user.domain.User;
+import com.carespoon.user.dto.UserMakeResponseDto;
 import com.carespoon.user.dto.UserSaveRequestDto;
 import com.carespoon.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -14,9 +17,9 @@ public class UserController {
 
     private final UserService userService;
     @PostMapping("/user")
-    public String save(@RequestBody UserSaveRequestDto requestDto){
+    public ResponseEntity<UserMakeResponseDto> save(@RequestBody UserSaveRequestDto requestDto){
         User user = userService.save(requestDto.getEmail(), requestDto.getName(), requestDto.getRole());
-        return user.getUuid();
+        return ResponseEntity.ok(new UserMakeResponseDto(user.getUuid()));
     }
     @GetMapping("/user/{uuid}")
     public User findByUUID(@PathVariable String uuid){
---
+++ carespoon/src/main/java/com/carespoon/user/dto/UserMakeResponseDto.java
@@ -0,0 +1,10 @@
+package com.carespoon.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserMakeResponseDto {
+    private String userId;
+}
